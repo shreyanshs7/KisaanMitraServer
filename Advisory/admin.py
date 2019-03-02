f4e3a82b4a4eb52e5e94dd7b95838cf14b191843
shreyashrivastava02 <shreyashrivastava02@yahoo.com>
2019-03-02 23:39:44 +0530

    updated admin models

--- Advisory/admin.py
+++ Advisory/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 from .models import Advice, AdviceCategory
 
+class AdviceAdmin(admin.ModelAdmin):
+    list_display=('id','user','title')
+    search_fields=('id','user','title')
+    list_filters=('user',)
+    ordering=('-created_at',)
+    readonly_fields=('created_at', 'updated_at')
+
+class AdviceCategoryAdmin(admin.ModelAdmin):
+    list_display=('id','advice','crop')
+    search_fields=('id','crop','advice')
+    list_filters=('user',)
+    ordering=('-created_at',)
+    readonly_fields=('created_at', 'updated_at')
 # Register your models here.
-admin.site.register(Advice)
-admin.site.register(AdviceCategory)
+admin.site.register(Advice,AdviceAdmin)
+admin.site.register(AdviceCategory,AdviceCategoryAdmin)
